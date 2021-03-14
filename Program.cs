d351b3a12642c7cb8d8145544036b4e2d3480a95
Ev <die_der_der_die@mail.ru>
2021-03-14 21:49:32 +0100

    extended convertToDouble to work in different environments, added filename input by user, added file exist check

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Text;
 
@@ -8,23 +9,39 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var filename = Path.Combine(currentDirectory, "data.csv");            
-            var filenameCalcExport = Path.Combine(currentDirectory, "data_calc_result.csv");
+            Console.WriteLine("Please, enter the name of the path, where .csv file is:"); 
+            var fileDirectory = Console.ReadLine(); // C:\Users\49178\source\repos\ConsoleAppTest1\ConsoleAppTest1
+            Console.WriteLine("Please, enter the name of the .csv file with an extension:");
+            var fileWithExtension = Console.ReadLine();
+            var filename = Path.Combine(fileDirectory, fileWithExtension);       // C:\Users\49178\source\repos\ConsoleAppTest1\ConsoleAppTest1\data.csv     
+            
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("The file exists.");
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist.");
+            }
+
+            var filenameCalcExport = Path.Combine(fileDirectory, "data_calc_result.csv");
             var lines = File.ReadAllLines(filename, Encoding.UTF8);                   
 
             for (int index = 1; index < lines.Length; index++)
             {
                 var line = lines[index];
-                var values = line.Split(";");                
-                var value1 = Convert.ToDouble(values[1]);
-                var value2 = Convert.ToDouble(values[2]);
+                var values = line.Split(";");
+
+                NumberFormatInfo provider = new NumberFormatInfo();
+                provider.NumberDecimalSeparator = ".";
+                var value1 = Convert.ToDouble(values[1], provider);
+                var value2 = Convert.ToDouble(values[2], provider);
                 var calcOperator = values[3];
 
                 var sumResult = value1 + value2;
                 var calcResult = CalcValuesWithOperator(value1, value2, calcOperator);
 
-                lines[index] = line + sumResult.ToString() + ";" + calcResult.ToString();                
+                lines[index] = line + sumResult.ToString() + ";" + calcResult.ToString(); //"1;763.98;765.87;+;"+"23"+";"+"400" = "1;763.98;765.87;+;23;400"               
             }
             
             File.WriteAllLines(filenameCalcExport, lines);
